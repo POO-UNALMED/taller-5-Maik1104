171a06e0c2b74f123895b6c140b52516186e9585
Michael Zapata <mizapataa@unal.edu.co>
2020-09-29 23:19:22 -0500

    Update Animal.java

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -75,7 +75,7 @@ public class Animal {
 	
 	
 	public static String totalPorTipo() {
-		return ("Mamiferos: "+ Mamifero.cantidadMamiferos() +"\nAves: "+ Ave.cantidadAves() + "\nReptiles: " + Reptil.cantidadReptiles() + "\nPeces: " + Pez.cantidadPeces() + "\nAnfibios: " + Anfibio.cantidadAnfibios());
+		return ("Mamiferos: "+ Mamifero.cantidadMamiferos() +"\n" + "Aves: "+ Ave.cantidadAves() + "\n"+"Reptiles: " + Reptil.cantidadReptiles() + "\n"+"Peces: " + Pez.cantidadPeces() + "\n"+"Anfibios: " + Anfibio.cantidadAnfibios());
 	}
 	
 	
